74d93d843d9414b36efc67be71d15d311182d037
Richard Littauer <richard.littauer@gmail.com>
2014-12-25 16:17:00 -0800

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+Practice
+====
+
+> “Practice makes the master.” - Patrick Rothfuss
+
+This is an attempt to have a daily to do list, which will display what hasn't been done today in the console. 
+
+The goal would be that there are several tasks which ought to be done daily: since I love a clean desktop, I print out these tasks (and these only) on the desktop using GeekTool and remove them as I complete them. I reset it daily. This way I am constantly reminded to practice. 
+
+### Install
+
+`npm install -g practice`
---
+++ index.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+"use strict";
+
+var fs = require('fs')
+var path = require('path')
+
+var argv = require('minimist')(process.argv.slice(2), {
+    alias: {
+        i: 'input',
+        d: 'done',
+        r: 'reset',
+        n: 'new'
+    }
+});
+
+var inputFile = argv.input || path.join(__dirname, '.', 'practice.txt')
+
+var tasks = JSON.parse(fs.readFileSync(inputFile, 'utf8'))
+
+function list () {
+  for (var key in tasks) {
+    if (tasks.hasOwnProperty(key)) {
+      if (!tasks[key]) {
+        console.log(key)
+      }
+    }
+  }
+  fs.writeFileSync(inputFile, JSON.stringify(tasks, null, 2))
+}
+
+function done (task) {
+  tasks[task] = true;
+}
+
+function reset () {
+  // TODO Reset tasks individually
+  for (var key in tasks) {
+    tasks[key] = false;
+  }
+}
+
+function newTask (task) {
+  tasks[task] = false;
+}
+
+if (argv.r) { reset(tasks) }
+if (argv.d) { done(argv.d) }
+if (argv.n) { newTask(argv.n) }
+list()
